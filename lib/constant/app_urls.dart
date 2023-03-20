7bb109cf1d4ae10b4d5a7c03cea7800b8d39d0ea
muzaffer72 <guzelim.batmanli@gmail.com>
2023-03-20 22:53:53 +0300

    Update app_urls.dart

--- lib/constant/app_urls.dart
+++ lib/constant/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
-  static const String baseUrl = 'https://onvao.net'; // 基础接口地址
+  static const String baseUrl = 'https://p.xflash.cc'; // 基础接口地址
   static const String baseApiUrl = '$baseUrl/api/v1'; // 基础接口地址
 
   static const String login = '$baseApiUrl/passport/auth/login';
